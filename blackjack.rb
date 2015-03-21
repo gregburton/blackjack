3ed975d5ac7fb2fdcefb21be6e4a5e9a296d8fdb
Greg Burton <gburt802@gmail.com>
2015-03-21 17:32:21 -0400

    Update blackjack.rb

--- blackjack.rb
+++ blackjack.rb
@@ -69,10 +69,10 @@ class Game
   def deal_cards
     @player_card_1 = @deck.pop
     @dealer_card_1 = @deck.pop
-    # in real life the dealer's first card is face down, so player can't see it (so don't immediately reveal what the card is to the player)
+    # dealer's first card is face down, so player can't see it (so don't immediately reveal to player)
     @player_card_2 = @deck.pop
     @dealer_card_2 = @deck.pop
-    # in real life, the dealer's second card is face up (so that both dealer and human player can see it). IN THE GAME: notify player what the dealer's card is
+    # dealer's second card is face up (so notify player what the dealer's card is)
   end
 
   def merge_cards_to_hands
@@ -83,13 +83,13 @@ class Game
 
   def valuing_ace
     if @player_hand || @dealer_hand < 11
-      #some code to change value of "A" = 11
+      # some code to change value of "A" = 11 based on scenario
     end
-    #but what happens if you get dealt two Aces? do both aces have to equal the same thing?
+    # but what happens if you get dealt two Aces? do both aces have to equal the same thing?
   end
 
-  def determine_hand_points #AKA adding the cards together (calls upon the valuing_ace method)
-    #not sure how I would be able to get valuations assigned to cards. This is an attempt of demonstrating how if I can get these valuations, that they could be added together using an array
+  def determine_hand_points # in other words, adding the card values together (calls upon the valuing_ace method)
+    # not sure how to get valuations assigned to cards--they could be added together using an array
     @player_hand_points = [val_player_card_1, val_player_card_2]
     @player_hand_points.inject{|sum,x| sum + x }
 
@@ -100,7 +100,7 @@ class Game
   def first_check_for_blackjack
     if @player_hand_points = 21 || @dealer_hand_points
       puts "You have blackjack. Congratulations!"
-        #skip over other steps to add winnings to pot and then ask if a new game is desired
+        # skip over other steps to add winnings to pot and then ask if a new game is desired
     elsif @player_hand_points = 21 && @dealer_hand_points = 21
       puts "Both you and the dealer have 21. A push (tie) has occurred."
     end
@@ -111,7 +111,7 @@ class Game
     puts "The dealer has #{dealer_card_2} and another card that's face-down. Once you make a decision to (s)tand or (h)it you'll get to find out what this mystery card is."
     @player_response = gets.chomp.downcase!
       if @player_response == "s"
-        #don't do anything else
+        # don't do anything else
       if @player_response == "h"
         @player_card_3 = @deck.pop
         puts "The dealer also has #{dealer_card_1}."
@@ -123,8 +123,8 @@ class Game
     hit = deck.pop
     deck.pop = @dealer_card_3
     if @dealer_hand_points < 17 == hit
-      #and then add value of @dealer_card_3 to @dealer_hand_points
-      #and inject @dealer_hand with third card
+      # add value of @dealer_card_3 to @dealer_hand_points
+      # and inject @dealer_hand with third card
       puts "The dealer also has #{dealer_card_3}"
     end
   end
@@ -132,12 +132,12 @@ class Game
   def if_player_hits
     @player_hand_points = "#{player_card1}, #{player_card2}, #{player_card3}].inject {|sum, n| sum + n }"
     if @player_hand_points > 21
-      puts "Bust. Game over." #and then needs to call up class NewGame
+      puts "Bust. Game over." # then needs to call up class NewGame
     elsif @player_hand_points < 21
     end
   end
 
-    #player could potentially hit again to receive a fourth card, so should allow for that possibility
+    # player could potentially hit again to receive a fourth card, so code should allow for that possibility
 
   def determining_winner
     if @player_hand_points = 21 && @dealer_hand_points < 21 || 21 > @player_hand_points > @dealer_hand_points || @dealer_hand_points > 21
@@ -153,9 +153,9 @@ class Game
     if @player_win
       @ending_pot = pot + bet + 15
       puts "Congratulations! You won $15 dollars!"
-      #if player loses, pot stays the same. there needs to be a way to carry over this balance to the next game (if another game is desired)
+      # if player loses, pot stays the same--there needs to be a way to carry over balance to the next game (if another game is desired)
     elsif @player_tie
-      @ending_pot = pot + bet #returns bet $ to player
+      @ending_pot = pot + bet # returns bet $ to player
       puts "You tied. Not bad. Not great, either...but not bad."
     else @dealer_win
       @ending_pot = pot - bet
@@ -170,9 +170,9 @@ class NewGame
     new_game_response = gets.chomp.downcase!
     if new_game_response == "y"
       Cards.new
-      #set loop to start the game over again. This will require collecting the cards and shuffling them again.)
+      # set loop to start the game over again. This will require collecting the cards and shuffling them again.)
     # if new_game_response == "n"
-    #   exit
+    # exit
     else 
       "Please specify (y)es or (n)o."
     end
